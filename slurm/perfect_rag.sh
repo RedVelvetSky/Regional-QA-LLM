7b60e6fafc1a2caad873ff464d2dcad3a131702e
Tommy Chu <tommychu2256@gmail.com>
2025-05-17 02:00:59 +0200

    update slurm pbs

--- slurm/perfect_rag.sh
+++ slurm/perfect_rag.sh
@@ -3,14 +3,14 @@
 #SBATCH --partition=gpu
 #SBATCH --output="logs/J-%x.out"
 #SBATCH --error="logs/J-%x.out"
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a100_80:1
 
 echo "SLURM WORKLOAD START: $(date)"
 start=$(date +%s)
 
 nvidia-smi
 
-module load Python/3.12.3-GCCcore-13.3.0
+#module load Python/3.12.3-GCCcore-13.3.0
 source .venv/bin/activate
 
 python src/e2e.py configs/perfect_rag.json
