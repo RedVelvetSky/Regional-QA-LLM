363d0dc965b75ffbe190fc39bff47cbbf869e444
Petr Kašpárek <petr@kasparek.cz>
2025-05-18 10:34:19 +0200

    Compute overall results

--- outputs/aggregate_results.py
+++ outputs/aggregate_results.py
@@ -4,7 +4,9 @@ import jsonlines
 from collections import defaultdict
 files = [
     "outputs/no_rag.jsonl",
-    "outputs/perfect_rag.jsonl",
+    "outputs/no_rag_en.jsonl",
+    "outputs/perfect_rag-2.jsonl",
+    "outputs/perfect_rag_en.jsonl",
     "outputs/yes_rag.jsonl",
 ]
 
@@ -17,27 +19,40 @@ def parse_llm_judge(evaluation):
     return evaluation["score"] >= LLM_JUDGE_THRESHOLD
 
 SCORE_FUNCTIONS = {
-    "BERTScore": lambda x: x["F1"],
-    "Phi4MiniLLMScore": parse_llm_judge,
-    "chrF": lambda x: x["score"],
-    "rougeL": lambda x: x["rougeL"]["fmeasure"],
+    "Phi4MiniLLMScore": lambda x: parse_llm_judge(x["Phi4MiniLLMScore"]),
+    "BERTScore_F1": lambda x: x["BERTScore"]["F1"],
+    "BERTScore_P": lambda x: x["BERTScore"]["P"],
+    "BERTScore_R": lambda x: x["BERTScore"]["R"],
+    "chrF": lambda x: x["chrF"]["score"],
+    "rougeL_F1": lambda x: x["rougeL"]["rougeL"]["fmeasure"],
+    "rougeL_P": lambda x: x["rougeL"]["rougeL"]["precision"],
+    "rougeL_R": lambda x: x["rougeL"]["rougeL"]["recall"],
 }
 
 
-def compare_strange_results(results):
-    for perfect, yes in zip (results["perfect_rag.jsonl"], results["yes_rag.jsonl"]):
-        perfect_score = parse_llm_judge(perfect["evaluation"]["Phi4MiniLLMScore"])
-        yes_score = parse_llm_judge(yes["evaluation"]["Phi4MiniLLMScore"])
-        if perfect_score < yes_score:
-            ...
+def compare_strange_results(results, expected_better, expected_worse):
+    total = 0
+    for better, worse in zip (results[expected_better], results[expected_worse]):
+        better_score = parse_llm_judge(better["evaluation"]["Phi4MiniLLMScore"])
+        worse_score = parse_llm_judge(worse["evaluation"]["Phi4MiniLLMScore"])
+        if better_score != worse_score:
+            total += 1
+
+    print(f"Total: {total}")
 
 
 def find_missing_context(results):
     czech_data = list(jsonlines.open("data/CZ.dev.jsonl"))
 
-    for line, czech in zip(results["perfect_rag.jsonl"], czech_data):
+    for line, czech in zip(results["perfect_rag-2.jsonl"], czech_data):
         if not line["context"][0]:
             print(czech["wikititle"])
+
+
+def has_context(line):
+    context = line["context"]
+    return (isinstance(context, str) and context != "") or \
+           (isinstance(context, list) and all(isinstance(c, str) and c != "" for c in context))
 
 
 def main():
@@ -45,8 +60,8 @@ def main():
     for file in files:
         results[file.split("/")[-1]] = list(jsonlines.open(file))
 
-    compare_strange_results(results)
-    find_missing_context(results)
+    # compare_strange_results(results, "no_rag_en.jsonl", "no_rag.jsonl")
+    # find_missing_context(results)
 
     for file in results:
         print(file)
@@ -54,16 +69,16 @@ def main():
 
         scores = defaultdict(int)
         count = 0
-        for line, perfect in zip(lines, results["perfect_rag.jsonl"]):
-            if not perfect["context"][0]:
+        for line, perfect in zip(lines, results["perfect_rag_en.jsonl"]):
+            if not has_context(perfect):
                 continue
 
             for score_name, score_func in SCORE_FUNCTIONS.items():
-                scores[score_name] += score_func(line["evaluation"][score_name])
+                scores[score_name] += score_func(line["evaluation"])
             count += 1
 
         for score_name, score in scores.items():
-            print(f"{score_name}: {score / count:.4f}")
+            print(f"{score_name}: {score / count:.3f}")
         print(f"Count: {count}")
         print()
 
